7ae0ecb39c4469cdc4936a22fad64de565c3a781
handrawanw <handrawanw@gmail.com>
2020-08-25 14:56:08 +0700

    perbaikan terakhir

--- Store/Redux/actionState/ButtonState.js
+++ Store/Redux/actionState/ButtonState.js
@@ -7,23 +7,24 @@ export function reducer(state=STORE_REDUX,action){
     case 'HAPUS_ALL_VALUE':
       return {...state,value:''};
     case 'TOTAL_ALL_VALUE':
-      if(state.value==="ANGKA MELEBIHI BATAS"){
-        return {...state,value:"ANGKA MELEBIHI BATAS"};
-      }else{
-        "use strict";
-        try{
+      "use strict";
+      try{
+      if(state.value==="ANGKA MELEBIHI BATAS")
+        if(state.value.length<=20){
           let total="";
           if(state.value.includes("x")){
             total=state.value.replace(/x/g,"*");
             return {...state,value:eval(total)};
           }
           return {...state,value:eval(state.value)};
-        }catch(e){
-          return {...state,value:'INPUT SALAH'};
+        }else{
+          return {...state,value:"ANGKA MELEBIHI BATAS"};
         }
+      }catch(e){
+        return {...state,value:'INPUT SALAH'};
       }
     case 'INPUT_VALUE':
-        return {...state,value:state.value.length<20?state.value+=action.value:"ANGKA MELEBIHI BATAS"};
+        return {...state,value:state.value+=action.value};
     default:
       return {...state};
   }
